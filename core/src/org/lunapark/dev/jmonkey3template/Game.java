78b7d926586f6478db5b41fffd06b6909dad9ea8
znak <tynuza@gmail.com>
2017-04-07 12:31:08 +0300

    Add desktop launcher

---
+++ core/src/org/lunapark/dev/jmonkey3template/Game.java
@@ -0,0 +1,27 @@
+package org.lunapark.dev.jmonkey3template;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+/**
+ * Basic game class
+ * Created by znak on 07.04.2017.
+ */
+
+public class Game extends SimpleApplication {
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+
+        rootNode.attachChild(geom);
+    }
+}
---
+++ desktop/src/org/lunapark/dev/jmonkey3template/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package org.lunapark.dev.jmonkey3template;
+/**
+ * Desktop launcher
+ * Created by znak on 07.04.2017.
+ */
+
+public class DesktopLauncher {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+}
